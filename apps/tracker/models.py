c4a15329c040e6f6f86e39c61daed22ff6e32669
Maxim Rezan <tvletsplay412@gmail.com>
2020-06-16 12:15:30 +0300

    first commit

---
+++ apps/tracker/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserBalance(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mdl = models.IntegerField(default=0)
+    eur = models.IntegerField(default=0)
+    usd = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.user.username + "'s balance"
---
+++ apps/tracker/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, 'tracker/home.html')
+
+@login_required
+def userPage(request):
+    return render(request, 'tracker/userpage.html')
+
+def signUpUser(request):
+    if request.method == "POST":
+        if request.POST.get('password1') == request.POST.get('password2'):
+            try:
+                user = User.objects.create_user(username=request.POST.get('username'),
+                                                password=request.POST.get('password1'),
+                                                first_name=request.POST.get('first_name'),
+                                                last_name=request.POST.get('last_name'))
+                user.save()
+                login(request, user)
+                return redirect('userPage')
+            except IntegrityError:
+                return render(request, 'tracker/signup.html', {'error':'That username has been already taken'})
+        else:
+            return render(request, 'tracker/signup.html', {'error':'Passwords do not match'})
+    else:
+        return render(request, 'tracker/signup.html')
+
+@login_required
+def logOutUser(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('home')
+
+def logInUser(request):
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST.get('username'),
+                                     password=request.POST.get('password'))
+        if user is None:
+            return render(request, 'tracker/login.html', {'error':'Username and password do not match'})
+        else:
+            login(request, user)
+            return redirect('userPage')
+    else:
+        return render(request, 'tracker/login.html')
